490f0920ec2519665f32c580b3e79f2ffc7d10b7
Michelle da Rosa Machado <machado.profile@gmail.com>
2022-06-28 16:54:33 -0300

    alguns updates

--- src/pages/tela-servicos/TelaServicos.js
+++ src/pages/tela-servicos/TelaServicos.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import BASE_URL from "../../constantes/BASE_URL"
+import axios from 'axios'
 
 export default class TelaServicos extends Component {
+
+  state = {
+
+  }
+
+  getAllJobs = () => {
+    axios.get(`${BASE_URL}/jobs`, {
+
+      headers: {
+        Authorization: "28d01bab-b426-4bdd-ba33-cf26427fc293"
+      }
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+  }
   
 
   render() {
+
+
     return (
       <div>TelaServicos</div>
     )
