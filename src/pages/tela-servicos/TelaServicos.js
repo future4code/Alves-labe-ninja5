c8728bcfd88b384696ce2999e55a6631a1bf3eec
Olavo-marques-do-nascimento <omarquesdonascimento@gmail.com>
2022-06-28 16:54:57 -0300

    Aparencia inicial filtro

--- src/pages/tela-servicos/TelaServicos.js
+++ src/pages/tela-servicos/TelaServicos.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
+import { InputsPai } from "./styled"
 
 export default class TelaServicos extends Component {
-  
+
+  state = {
+    precoMinimo: "",
+    precoMaximo: "",
+    buscaNome: ""
+  }
+
+  onPrecoMinimo = (event) => {
+    console.log("entrou preco precoMinimo")
+    this.setState({ precoMinimo: event.target.value })
+  }
+
+  onPrecoMaximo = (event) => {
+    console.log("entrou preco precoMaximo")
+    this.setState({ precoMaximo: event.target.value })
+  }
+
+  onBuscaNome = (event) => {
+    console.log("entrou preco buscaNome")
+    this.setState({ buscaNome: event.target.value })
+  }
+
+
 
   render() {
     return (
-      <div>TelaServicos</div>
+      <InputsPai>
+        <div>
+          Filtro Mínimo:
+          <input
+            type="number"
+            onChange={this.onPrecoMinimo}
+            value={this.state.precoMinimo}
+            placeholder="Valor Mínimo"
+            />
+        </div>
+        <div>
+          Filtro Máximo:
+          <input
+            type="number"
+            onChange={this.onPrecoMaximo}
+            value={this.state.precoMaximo} 
+            placeholder="Valor Máximo"
+            />
+        </div>
+        <div>
+          Busca por Nome:
+          <input
+            type="search"
+            onChange={this.onBuscaNome}
+            value={this.state.buscaNome}
+            placeholder="Buscar Nome"
+            />
+        </div>
+      </InputsPai>
     )
   }
 }
+
---
+++ src/pages/tela-servicos/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const InputsPai = styled.div`
+border: 1px solid black;
+display:flex;
+
+`
