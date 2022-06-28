dd35801756b4301bfcba9c5467e4264042ee8eb9
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-06-28 20:10:11 -0300

    alteracoes gerais no app

--- src/App.js
+++ src/App.js
@@ -1,76 +1,14 @@
 import React from "react";
-import TelaInicial from "./pages/tela-inicial/TelaInicial"
-import TelaServicos from "./pages/tela-servicos/TelaServicos"
-import TelaCadastro from "./pages/tela-cadastro/TelaCadastro"
-import TelaDetalhe from "./pages/tela-detalhe/TelaDetalhe"
-import TelaCarrinho from "./pages/tela-carrinho/TelaCarrinho"
-
+import Home from "./pages/home/home";
 export default class App extends React.Component {
-
-  state = {
-
-    paginaAtual: "tela-cadastro"
-
-  }
-
-  procuraPagina = () => {
-    switch (this.state.paginaAtual) {
-      case "tela-inicial":
-        return <TelaInicial 
-          goToTelaCadastro={this.goToTelaCadastro} 
-          goToTelaServicos={this.goToTelaServicos} 
-          />
-
-      case "tela-cadastro":
-        return <TelaCadastro />
-
-      case "tela-servicos":
-        return <TelaServicos />
-
-      case "tela-detalhe":
-        return <TelaDetalhe />
-
-      case "tela-carrinho":
-        return <TelaCarrinho />
-
-      default:
-        return "Erro ao Encontrar pagina"
-
-    }
-  }
-
-  goToTelaInicial = () => {
-    this.setState({ paginaAtual: "tela-inicial" })
-  }
-
-  goToTelaCadastro = () => {
-    this.setState({ paginaAtual: "tela-cadastro" })
-  }
-
-  goToTelaServicos = () => {
-    this.setState({ paginaAtual: "tela-servicos" })
-  }
-
-  goToTelaDetalhe = () => {
-    this.setState({ paginaAtual: "tela-detalhe" })
-  }
-
-  goToTelaCarrinho = () => {
-    this.setState({ paginaAtual: "tela-carrinho" })
-  }
 
   render() {
 
     return (
+      <Home />
 
-      <div>
-        {this.procuraPagina()}
-      </div>
-    );
+    )
 
   }
 
 }
-
-
-
